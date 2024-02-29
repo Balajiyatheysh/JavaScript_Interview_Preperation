0d695d1e048125b91be4cda27acc1ced73e6bcd5
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-02-29 09:08:39 +0530

    callback map filter and reduce

---
+++ callbacks/main.js
@@ -0,0 +1,23 @@
+const makecake = (budget) =>{
+  const totalBudget = budget *20;
+  function buyIngredients (totalBudget){
+    const utensilsBudget = totalBudget/5;
+    function purchaseUtensils (utensilsBudget){
+      function bakeCake (oven, knife, plates){ 
+        function decorateCake (chacolates, cream, candles, ballons){
+          return cake;
+        }
+      }
+    }
+  }
+}
+
+function callback (cbFunc){
+  cbFunc();
+}
+
+function childFunc (){
+  console.log("hey there");
+}
+
+callback(childFunc);
--- map-filter-reduce/map.js
+++ map-filter-reduce/map.js
@@ -1,7 +1,9 @@
 const arr1 = [1,2,3,4,5];
-const mappedArray = arr1.map(arr =>arr *2);
-const filteredArray = arr1.filter(arr =>arr %2 === 0);
+
+const mappedArray = arr1.map(arr => arr * 2 );
+const filteredArray = arr1.filter(arr => arr % 2 === 0);
 const reducedArray = arr1.reduce((accumulator, current)=>accumulator + current, 5);
+
 console.log(mappedArray);
 console.log(filteredArray);
 console.log(reducedArray);
