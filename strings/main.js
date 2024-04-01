2b68bf2a1ae760c1b7fdf3d40f10ed53d6144e80
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-04-01 22:18:54 +0530

    1-4-2024

---
+++ strings/main.js
@@ -0,0 +1,13 @@
+const truncate = (str, maxlength) => {
+  if (str.length > maxlength) {
+    let str2 = str.slice(0, maxlength);
+    return str2.concat("...");
+  } else {
+    return str;
+  }
+};
+let str = "Welcome to my channel";
+console.log(truncate(str, 7));
+
+//automic structure pattern
+//backend for frontends
