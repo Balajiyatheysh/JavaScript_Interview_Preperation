6ba8521eb7c4f6bfa513ad1815a56d5798c1c44c
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-04-24 10:34:20 +0530

    24-4-2024

--- map-filter-reduce/map.js
+++ map-filter-reduce/map.js
@@ -1,7 +1,8 @@
+
 const arr1 = [1,2,3,4,5];
 
 const mappedArray = arr1.map(arr => arr * 2 );
-const filteredArray = arr1.filter(arr => arr % 2 === 0);
+const filteredArray = arr1.filter(arr => arr %2 ===0);
 const reducedArray = arr1.reduce((accumulator, current)=> { return accumulator + current}, 5);
 console.log(mappedArray);
 console.log(filteredArray);
